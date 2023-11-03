012f079f590d37c42991d6d199815d9b7cc01d92
Rajnish Kumar <rjrajnish1729@gmail.com>
2023-11-03 15:36:10 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,47 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+    options: ["New York", "London", "Paris", "Dublin"]
+  },
+    {
+    numb: 2,
+    question: "Who painted the Mona Lisa?",
+    answer: "Leonardo Da Vinci",
+    options:  ["Vincent Van Gogh", "Pablo Picasso", "Leonardo Da Vinci", "Claude Monet"]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  }
+];
